a0ab8973ec52696983bbedb898f9a3c5de662f95
Adrián <adrims.0626@gmail.com>
2026-01-31 11:02:28 +0100

    Cambio de color del relleno a verde

--- Assets/_Test_/VidaPersonaje/ScriptVida.cs
+++ Assets/_Test_/VidaPersonaje/ScriptVida.cs
@@ -6,27 +6,27 @@ public class ScriptVida : MonoBehaviour
 
     public int maxVida = 5;
     public int vida;
-    public SliderVida slidervida;
+    public SliderVida sliderVida;
     public Boolean damage = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         vida = maxVida;
-        slidervida.SetVidaMax(maxVida);
+        sliderVida.SetVidaMax(maxVida);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(damage)
+        if(damage) //Al recibir daño
         {
             reducirSalud();
         }
     }
 
-    public void reducirSalud()
+    public void reducirSalud() //Reduce la salud y la barra
     {
         vida--;
-        slidervida.SetSlider(vida);
+        sliderVida.SetSlider(vida);
     }
 }
